37b861ebfbdbb3ceff298955e9d046b06d31744a
tania <20955072@students.latrobe.edu.au>
2023-08-29 16:53:07 +1000

    made phone notNull

--- api/models/phone.model.js
+++ api/models/phone.model.js
@@ -1,11 +1,13 @@
 module.exports = (sequelize, Sequelize) => {
     const Phone = sequelize.define("phone", {
         id: {
+            allowNull:false,
             type: Sequelize.INTEGER,
             autoIncrement: true,
             primaryKey: true,
         },
         name: {
+            allowNull:false,
             type: Sequelize.STRING,
             number: Sequelize.STRING,
             contactId: Sequelize.INTEGER,

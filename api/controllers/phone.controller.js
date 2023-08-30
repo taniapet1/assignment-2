4ae5be688e7f26ee3bee4fffb3b2c0736f2ae24c
tania <20955072@students.latrobe.edu.au>
2023-08-30 11:59:12 +1000

    Added Phone controller

--- api/controllers/phone.controller.js
+++ api/controllers/phone.controller.js
@@ -4,25 +4,103 @@ const Op = db.Sequelize.Op;
 
 // Create phone
 exports.create = (req, res) => {
-    
+    Phone.create()
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || "An error has occurred"
+        });
+    });
 };
 
 // Get all phones
 exports.findAll = (req, res) => {
+    const id = req.params.id;
     
+    Phone.findAll()
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || "An error has occurred"
+        });
+    }); 
 };
 
 // Get one phone by id
 exports.findOne = (req, res) => {
-  
+    const id = req.params.id;
+
+    Phone.findOne({
+        where: { id: id }
+    })
+    .then(num => {
+        if (num == 1) {
+            res.send({
+                message: "Task was deleted successfully!"
+            });
+        } else {
+            res.send({
+                message: `Cannot delete Task`
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: "Could not delete Task with id=" + id
+        });
+    });
 };
 
 // Update one phone by id
 exports.update = (req, res) => {
-    
+    const id = req.params.id;
+
+    Phone.update({
+        where: { id: id }
+    })
+    .then(num => {
+        if (num == 1) {
+            res.send({
+                message: "Task was deleted successfully!"
+            });
+        } else {
+            res.send({
+                message: `Cannot delete Task`
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: "Could not delete Task with id=" + id
+        });
+    });
 };
 
 // Delete one phone by id
 exports.delete = (req, res) => {
-    
+    const id = req.params.id;
+
+    Phone.destroy({
+        where: { id: id }
+    })
+    .then(num => {
+        if (num == 1) {
+            res.send({
+                message: "Phone number was deleted successfully!"
+            });
+        } else {
+            res.send({
+                message: `Cannot delete number`
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: "Could not delete Task with id=" + id
+        });
+    });
 };

922d5f2bc05f61553262cbb455eeefde90fb654f
tania <20955072@students.latrobe.edu.au>
2023-08-30 11:22:22 +1000

    Added Contact controller file

--- api/controllers/contact.controller.js
+++ api/controllers/contact.controller.js
@@ -4,26 +4,83 @@ const Phones = db.phones;
 const Op = db.Sequelize.Op;
 
 // Create contact
-exports.create = (req, res) => {
-    
+exports.create = (req, res) => { => {
+    Contact.create()
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || "An error has occurred"
+        });
+    });
 };
 
-// Get all contacts
+
+// Get all contacts   Lab8 task controller.js
 exports.findAll = (req, res) => {
-    
+    Contacts.findAll()
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || "An error has occurred"
+        });
+    }); 
 };
+
 
 // Get one contact by id
 exports.findOne = (req, res) => {
+    Contacts.findOne()
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || "An error has occurred"
+        });
+    });
+};
+
   
 };
 
 // Update one contact by id
 exports.update = (req, res) => {
-    
+    const id = req.params.id;
+
+    Contacts.update(req.body, {
+         where: { id: id }
+    })
+    .then(num => {
+        if (num == 1) {
+            res.send({
+                message: "Task was updated successfully."
+            });
+        } else {
+            res.send({
+                message: `Cannot update Task`
+            });
+        }
+    })
+    .catch(err => {
+       res.status(500).send({
+           message: "Error updating Task with id=" + id
+       });
+    }); 
 };
 
 // Delete one contact by id
 exports.delete = (req, res) => {
-    
+    Contacts.delete()
+    .then(data => {
+        res.send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || "An error has occurred"
+        });
+    });
 };
